c358dd6b268c5e263b9042bcc049205f5e4ba4cf
AndrewSushyi <sushyi_av@ukr.net>
2015-12-21 13:17:57 +0200

    add projects 2

---
+++ projects/company/js/myJS.js
@@ -0,0 +1,79 @@
+/*
+//alert( location.href );
+var allImg = document.getElementsByTagName('img');
+alert("value img = " + allImg.length);
+alert(allImg[2]);*/
+
+////////////////
+
+/*
+ var h = document.getElementsByClassName('banner');
+ alert(h.background);*/
+
+/*
+var h = document.getElementsByClassName('banner-wrapper');
+alert(h.width);*/
+////////////////
+
+/*var h = document.getElementById('div_for_img');
+var childDiv = h.childNodes;
+alert (childDiv.length);
+for (var i=0; i<childDiv.length; i++){
+    alert(childDiv[i].alt);
+}*/
+
+
+/////////////////
+
+/*
+var h = document.getElementById('div_for_img');
+var parentchildDiv = h.parentNode;
+alert (parentchildDiv.id);*/
+
+//////////////////////!!!!!!!! не видит #текст/
+/*
+var h = document.getElementById('div_for_img');
+var brotherDivPrev = h.previousElementSibling;
+var brotherDivNext = h.nextElementSibling;
+alert(brotherDivPrev);
+alert(brotherDivNext);
+*/
+
+
+////////////////////
+
+/*
+var h = document.getElementById('under_P');
+alert(h.innerText);
+alert(h.textContent);*/
+
+///////////
+
+/*
+var h = document.getElementById('div_for_img');
+h.innerHTML = "<p>test</p>";
+alert(h.innerHTML);
+*/
+
+
+//////////////////////////
+
+/*for (var i = 0; i < document.body.childNodes.length; i++) {
+    alert( document.body.childNodes[i] ); // Text, DIV, Text, UL, ..., SCRIPT
+}*/
+////////////////
+
+/*
+for (var i = 0; i < document.body.children.length; i++) {
+    alert( document.body.children[i] ); // DIV, UL, DIV, SCRIPT
+}
+*/
+
+//zadanie
+/*
+var head1 = document.documentElement.firstElementChild;
+var ul1 = document.body.getElementsByTagName(ul);
+alert(ul);
+*/
+
+
